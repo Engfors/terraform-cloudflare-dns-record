d29bee8b87dff35f98551b586841f3e9c623c7a6
Emil Engfors <emil.engfors@devoteam.com>
2021-03-15 14:03:39 +0100

    changed cloudflare version

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "2.19.2"
+      version = ">= 2.19"
     }
   }
   required_version = ">= 0.14"
